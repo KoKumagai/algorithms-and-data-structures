34057d71edbe70e4ef7f83e20201674c1e32a683
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-10-10 19:38:00 -0400

    Create Triangle3

---
+++ triangle/Triangle3.java
@@ -0,0 +1,16 @@
+package triangle;
+
+public class Triangle3 {
+    public static final int SIZE = 5;
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= SIZE; i++) {
+            for(int j = SIZE; j >= 1; j--) {
+                if(j >= i) {
+                    System.out.print("*");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
