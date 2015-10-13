c30fb024c62c07693ac45f5f927314556e6fb303
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-10-12 20:10:22 -0400

    Use constant instead of magic number

--- triangle/Triangle1.java
+++ triangle/Triangle1.java
@@ -1,9 +1,10 @@
 package triangle;
 
 public class Triangle1 {
+	public static final int SIZE = 5;
     
     public static void main(String[] args) {
-        for(int i = 1; i <= 5; i++){
+        for(int i = 1; i <= SIZE; i++){
             for(int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
