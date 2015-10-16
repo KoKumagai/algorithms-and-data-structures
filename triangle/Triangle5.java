8a02fd60d3c6e9280b2430e564358b690b261493
Ko Kumagai <ko.kumagai.ko@gmail.com>
2015-10-16 01:09:54 -0400

    Refactor Triangle5

--- triangle/Triangle5.java
+++ triangle/Triangle5.java
@@ -4,18 +4,14 @@ public class Triangle5 {
     public static final int SIZE = 5;
 
     public static void main(String[] args) {
-        int max = (SIZE - 1) * 2 + 1;
         for(int i = 0; i < SIZE; i++) {
-            int num = 2 * i + 1;
-            
-            for(int j = 0; j < (max-num) / 2; j++) {
-               System.out.print(" ");
+            for(int j = 0; j < SIZE - (i + 1); j++) {
+                System.out.print(" ");
             }
-            
-            for(int j = 0; j < num; j++) {
+
+            for(int j = 0; j < (i + 1) * 2 - 1; j++) {
                 System.out.print("*");
             }
-            
             System.out.print("\n");
         }
     }
